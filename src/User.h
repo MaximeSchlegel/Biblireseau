34b21450c88bd0d98c9629aa563d545665e56e75
Mimoun Mohamed <mim.mohamed33@gmail.com>
2018-12-11 17:15:42 +0100

    User non fini

---
+++ src/User.h
@@ -0,0 +1,19 @@
+//
+// Created by mimmo on 11/12/2018.
+//
+
+#ifndef BIBLIRESEAU_USER_H
+#define BIBLIRESEAU_USER_H
+#include<string>
+using namespace std;
+
+class User {
+private:
+    int id ;
+    string name ;
+    string surname ;
+
+};
+
+
+#endif //BIBLIRESEAU_USER_H
