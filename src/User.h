ef8afc24aa274f5318081d5bf6201f8463cddfc4
Mimoun Mohamed <mim.mohamed33@gmail.com>
2018-12-11 17:31:05 +0100

    Attributs de User et Book Classes

--- src/User.h
+++ src/User.h
@@ -5,14 +5,24 @@
 #ifndef BIBLIRESEAU_USER_H
 #define BIBLIRESEAU_USER_H
 #include<string>
+#include "Entity.h"
+#include "Library.h"
+#include "Book.h"
+#include <vector>
+
 using namespace std;
 
-class User {
+class User : public Entity {
 private:
+    static int created ;
     int id ;
     string name ;
     string surname ;
-
+    string address ;
+    string telephone ;
+    Library registered ;
+    int maxBorrowing ;
+    std::vector<Book> bookList ;
 };
 
 
