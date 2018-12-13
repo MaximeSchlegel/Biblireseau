014bab5e8fc1bd69697a39ac6891d16c37aed5ff
MaximeSchlegel <maxime.schlegel.83@gmail.com>
2018-12-13 12:45:17 +0100

    Coorection de Bug

--- src/User/User.cpp
+++ src/User/User.cpp
@@ -5,10 +5,14 @@ int User::created = 0;
 
 User::User(bool isEmpty) {
     if (isEmpty) {
-        this->id = 0;
-    }
-    else {
         User::created++;
         this->id = User::created;
     }
+    else {
+        this->id = 0;
+    }
+}
+
+int User::getId() {
+    return id;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "Book/Book.h"
 #include "User/Library.h"
-#include "User/User.h"
+#include "User/Member.h"
 
 #include <iostream>
 using namespace std;
@@ -8,5 +8,8 @@ using namespace std;
 
 int main() {
 	Library library1 = Library("MyFirstLibrary", "Ici");
+	cout << library1.getId() << endl;
+	cout << library1.getName() << endl;
+	cout << library1.getAddress() << endl;
 	return 0;
 }
