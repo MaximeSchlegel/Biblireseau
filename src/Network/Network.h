1504fb85e4fdeee4757014fe9d984f12391934de
Mimoun Mohamed <mim.mohamed33@gmail.com>
2019-01-14 22:37:15 +0100

    Afficahge en cours

--- src/Network/Network.h
+++ src/Network/Network.h
@@ -15,6 +15,13 @@ private:
     map<int,Library> libraryList;
     map<int,Member> memberList;
     map<int,Book> bookList;
+    void main_menu() ;
+    void member_menu();
+    void library_menu();
+    void member_authentificated_menu(Member member);
+    void member_borrow_menu(Member member);
+    void member_return_menu(Member member);
+    void clear();
 
 public:
     Network();
